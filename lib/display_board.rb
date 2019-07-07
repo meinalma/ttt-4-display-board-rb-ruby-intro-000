ca488c8482f5346921979e23a8c1f1f89db396e1
Dalma Lipcsei <lipcsei.dalma@gmail.com>
2019-07-07 13:52:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = [" "," "," "," "," "," "," "," "," "]
+board = [" ","X","0","]
 
 def display_board(board)
   puts "   |   |   "
@@ -11,4 +11,4 @@ def display_board(board)
   puts "   |   |   "
 end
 
-display_board(board)
+puts
